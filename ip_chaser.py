f83969d3dfae6e4583d3200a30bcf913226e2f1a
littinrajan <littinrajan@gmail.com>
2021-01-24 22:10:29 +0530

    Added IP Chaser Core Python File

---
+++ ip_chaser.py
@@ -0,0 +1,15 @@
+"""IP Chaser Package."""
+
+#Package: IP Chaser
+#Author: Littin Rajan
+
+
+
+#function to get help
+def helpme():
+    print("""
+    IP Chaser is a simple package for getting information about our system IP and any IP that we query
+    
+    Author: Littin Rajan            
+    ----------------------------------------------Enjoy IP Chaser---------------------------------------------
+    """)
